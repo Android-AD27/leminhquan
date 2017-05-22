2a030d42ecbea9beb48b623b19a46a1287d05780
Le Minh Quan <lequan.moon@gmail.com>
2017-05-22 15:58:15 +0700

    finish layout demo

---
+++ LayoutDemo/app/src/main/java/com/example/mypc/layoutdemo/ArticleUp.java
@@ -0,0 +1,13 @@
+package com.example.mypc.layoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticleUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_up);
+    }
+}

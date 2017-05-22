735ac6be9a7ca12c112ed37d477c74d2e0699ff5
Le Minh Quan <lequan.moon@gmail.com>
2017-05-22 10:52:39 +0700

    layout demo

---
+++ LayoutDemo/app/src/main/java/com/example/mypc/layoutdemo/car_detail.java
@@ -0,0 +1,13 @@
+package com.example.mypc.layoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class car_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_detail);
+    }
+}
